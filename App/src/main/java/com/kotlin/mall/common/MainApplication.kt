0a36240fdcce7b90cf5391567fa16eeb164c5691
feng <yangshirong@log56.com>
2018-04-18 15:44:35 +0800

    服务器地址修改

--- App/src/main/java/com/kotlin/mall/common/MainApplication.kt
+++ App/src/main/java/com/kotlin/mall/common/MainApplication.kt
@@ -1,5 +1,7 @@
 package com.kotlin.mall.common
 
+import android.content.Context
+import android.support.multidex.MultiDex
 import cn.jpush.android.api.JPushInterface
 import com.kotlin.base.common.BaseApplication
 
@@ -9,10 +11,15 @@ import com.kotlin.base.common.BaseApplication
 class MainApplication:BaseApplication() {
     override fun onCreate() {
         super.onCreate()
-
+        MultiDex.install(this);
         //极光推送初始化
         JPushInterface.setDebugMode(true)
         JPushInterface.init(this)
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }
 
